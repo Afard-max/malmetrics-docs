1c8ce10cae570ce10abb573fee3ac26930ec32f2
RuizDiazAlexander <af.alexander.rd@gmail.com>
2026-03-04 18:47:46 -0300

    Deploy website - based on cc20df48ff778a64b9711cd65c18b94bc2a950af

---
+++ assets/js/a7456010.e6789f13.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmalmetrics_docs=globalThis.webpackChunkmalmetrics_docs||[]).push([[235],{8552(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
---
+++ assets/js/aba21aa0.5d4cf66d.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunkmalmetrics_docs=globalThis.webpackChunkmalmetrics_docs||[]).push([[742],{7093(s){s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
